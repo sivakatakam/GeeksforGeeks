7eee84fa61ccd1e07ae6ad06a95cdc26ccac7f74
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-30 22:51:17 +0530

    Add files via upload

---
+++ Count Digits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int c=0,r=0,n=N;
+        while(n!=0)
+        {
+            r=n%10;
+            if(r!=0&&N%r==0)
+            c++;
+            n=n/10;
+        }
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
