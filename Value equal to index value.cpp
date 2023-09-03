efcc66f165f50bbe56960a5b27ac7013bf592c60
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-09-03 19:58:02 +0530

    Add files via upload

---
+++ Value equal to index value.cpp
@@ -0,0 +1,17 @@
+//User function template for C++
+class Solution{
+public:
+
+	vector<int> valueEqualToIndex(int arr[], int n) {
+	    // code here
+	    vector <int> v ;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]==i+1)
+	        v.push_back(i+1);
+	       
+	    }
+	     return v;
+	    
+	}
+};
