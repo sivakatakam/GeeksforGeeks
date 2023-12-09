4645dc5492aa274b05e95a73c863ade1f7ff6026
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-12-09 19:35:33 +0530

    Add files via upload

---
+++ Simple Interest.py
@@ -0,0 +1,19 @@
+#User function Template for python3
+
+class Solution:
+    def simpleInterest(self,A,B,C):
+        #code here
+        return (A * B * C) / 100
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        P,R,T=map(int,input().strip().split(' '))
+        ob=Solution()
+        print('%.2f'%ob.simpleInterest(P,R,T))
+# } Driver Code Ends
